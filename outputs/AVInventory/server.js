f8fcf0884b4d7e8d14cb905882cc8903077bb316
Jonah Lablanc <jonahandkennedy@gmail.com>
2026-06-24 14:00:56 -0700

    Prevent stale frontend assets after deploy

--- outputs/AVInventory/server.js
+++ outputs/AVInventory/server.js
@@ -484,7 +484,12 @@ function serveStatic(req, res, url) {
       ".js": "text/javascript; charset=utf-8",
       ".json": "application/json; charset=utf-8"
     };
-    res.writeHead(200, { "Content-Type": types[ext] || "application/octet-stream" });
+    res.writeHead(200, {
+      "Content-Type": types[ext] || "application/octet-stream",
+      "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+      "Pragma": "no-cache",
+      "Expires": "0"
+    });
     res.end(content);
   });
 }
